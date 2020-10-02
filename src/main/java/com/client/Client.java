00a2c7369f6a471c7eb87c09dc6f17b9d1d51355
CesarCouchinho <cesar.couchinho@gmail.com>
2020-10-02 15:15:39 +0100

    client

--- src/main/java/com/client/Client.java
+++ src/main/java/com/client/Client.java
@@ -1,6 +1,5 @@
 package com.client;
 
-import java.awt.List;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,10 +7,10 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.Properties;
 
 public class Client {
+	
 /*
  * 1-cliente liga e conecta-se a um server random
  * 			se nao for lider o follower rencaminha po lider e este faz o pedido de novo
@@ -23,8 +22,8 @@ public class Client {
  * 
  * 
  * */
-	private String port,clusterString,timeOutIntervalString;
-
+	private String port,clusterString,timeOutIntervalString,localhost;
+	private String clientID;
 	private ObjectOutputStream outToServer;
 	private ObjectInputStream inFromServer;
 	
@@ -37,14 +36,17 @@ public class Client {
 	
 	private void readIni() {
 		port="";
+		localhost="";
 		clusterList = new ArrayList<>();
 		try {
 			Properties p = new Properties();
 			p.load(new FileInputStream("src/main/resources/config.ini"));
 			
 			port = p.getProperty("port");
+			localhost = p.getProperty("ip");
 			clusterString = p.getProperty("cluster");
 			timeOutIntervalString = p.getProperty(timeOutIntervalString);
+			clientID = localhost + port;
 		
 		} catch (IOException e) {
 			System.err.println("Port : -> " + port + "\nClusterString : -> " + clusterString + "\n timeOutIntervalString : -> " +timeOutIntervalString);
@@ -57,24 +59,60 @@ public class Client {
 
 	}
 
+	
+	public String generateFullLog(String log) {
+		
+		String logID = clientID + System.currentTimeMillis();
+		
+		String generatedLog = logID + "-" + log;
+		
+		return generatedLog;
+	}
+	
+	
+	
+	public String request (String log) {
+		String stringReceived = "";
+		Object objectReceived;
+		try {
+		
+			outToServer.writeObject(generateFullLog(log));
+			
+			//TODO: Timeout
+		
+			objectReceived = inFromServer.readObject();
+			stringReceived = (String) objectReceived;
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return stringReceived;
+		
+	}
+	
+	
 	public void connectToServer() {
 		Socket clientSocket;
 		try {
 			InetAddress host = InetAddress.getLocalHost();
 			clientSocket = new Socket(host, Integer.parseInt(port));
 
-			outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
 			inFromServer = new ObjectInputStream(clientSocket.getInputStream());
-
-			outToServer.writeObject("client");
+			
+			outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
+			
+//			outToServer.writeObject("client");
 
 			while (true) {
-				try {
-					Object o = inFromServer.readObject();
-					
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				}
+				
+				String message = "";
+				request(message);
+				
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
